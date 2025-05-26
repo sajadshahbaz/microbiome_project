9d27f69ecfba1dbca218b654f04a7452e6b8648b
p100mma <p.stomma@uwb.edu.pl>
2025-05-26 15:19:48 +0200

    agp pipeline in progress

--- FS_functions.R
+++ FS_functions.R
@@ -52,80 +52,6 @@ filter_sparse_cols<- function(data, thr) {
 }
 
 
-
-#function performs 1D & 2D analysis jointly
-#for 2 discretization methods for features in X (based on 0, or median of each col in X)
-#
-# mc - minimum binarized feature class size, if it is less than mc, feature is filtered out.
-#
-#output of each of the 4 analysis variants
-#is stored in a corresponding sublist:
-#res_1Dm, res_2Dm, res_1D0, res_2D0 (two for median based and two for zero based)
-#
-#each sublist contains the fields mentioned at the beginning of the file.
-#
-#====================================================================================
-#====================================================================================
-#====================================================================================
-### @TO DO: estabilish a way of determining the synergy threshold from one run!
-#	    right now, it is only possible to include below only "post-hoc",
-#		after we estabilish robust set of 1D and 2D relevant features:
-#
-######moreover, each of the 2D sublists contains additional components:
-######	- partner_df: data.frame with one row per each feature in rel_set
-######		      where information on the best fitting, synergistic partner is given
-######	- partner_set:names of features that were found as synergistic partners,
-######		      but not found relevant by themselves
-#====================================================================================
-#====================================================================================
-#====================================================================================
-
-MDFS_FS<- function(X, y, lvl=0.05,
-		   p.adjust.method="holm",
-		   mc=30){
-   attributes(y)<-NULL
-   binarize_taxa(X,binarize_on_median = TRUE)|> filter_sparse_cols(thr=mc) -> Xm
-   
-   binarize_taxa(X,binarize_on_median = FALSE)|> filter_sparse_cols(thr=mc) -> X0
-   to_keep<- c("statistic","p.value","adjusted.p.value","relevant.variables")
-   MDFS.discrete(data=Xm,decision = y,dimensions = 1,
-                      p.adjust.method = p.adjust.method,
-		      level=0.05,seed=123)[to_keep]-> res_1Dm
-   MDFS.discrete(data=Xm,decision = y,dimensions = 2,
-                      p.adjust.method = p.adjust.method,
-		      level=0.05,seed=123)[to_keep]-> res_2Dm
-   MDFS.discrete(data=X0,decision = y,dimensions = 1,
-                      p.adjust.method = p.adjust.method,
-		      level=0.05,seed=123)[to_keep]-> res_1D0
-   MDFS.discrete(data=X0,decision = y,dimensions = 2,
-                      p.adjust.method =p.adjust.method, 
-		      level=0.05,seed=123)[to_keep]-> res_2D0
-  
-   res_1Dm$rel_set= colnames(Xm)[res_1Dm$relevant.variables]
-   res_2Dm$rel_set= colnames(Xm)[res_2Dm$relevant.variables]
-   res_1D0$rel_set= colnames(X0)[res_1D0$relevant.variables]
-   res_2D0$rel_set= colnames(X0)[res_2D0$relevant.variables]
-
-#   res_2Dm$partner_df= get_interaction_partner(dataset=Xm, decision=y,
-#					       	interesting.vars= res_2Dm$relevant.variables)
-#   res_2Dm$partner_df= find_true_partners( partner_data_frame= res_2Dm$partner_df, 
-#					   MDFS_1D_result= res_1Dm)
-#   res_2Dm$partner_set=only_partners(annotated_partner_df= res_2Dm$partner_df, res_1Dm)
-
-#   res_2D0$partner_df= get_interaction_partner(dataset=X0, decision=y,
-#					       	interesting.vars= res_2D0$relevant.variables)
-#   res_2D0$partner_df= find_true_partners( partner_data_frame= res_2D0$partner_df, 
-#					   MDFS_1D_result= res_1D0)
-#   res_2D0$partner_set=only_partners(annotated_partner_df= res_2D0$partner_df, res_1D0)
-
-   RESULT<-list(
-     res_1Dm=res_1Dm,
-     res_2Dm=res_2Dm,
-     res_1D0=res_1D0,
-     res_2D0=res_2D0
-   )
-   
-}
 #helper function for MDFS 2D:
 # given input data and result of a feature selection, 
 # function finds the best fitting synergistic partner to each
@@ -167,7 +93,7 @@ do.call(rbind,
 #               in comparison with 1D
 # synergy - TRUE, if IG_increase > synergy_thr (which if left null, is by default 
 #						set to minimum value of stat in 1D test
-#						    of 2D relevan features)
+#						    of 1D relevant features)
 
 find_true_partners<- function(partner_data_frame, MDFS_1D_result,
 			      synergy_thr= NULL
@@ -177,7 +103,7 @@ find_true_partners<- function(partner_data_frame, MDFS_1D_result,
 
 
   stat_1D_rel2D<- MDFS_1D_result$statistic [ partner_data_frame$rel_IDX ]
-  if(is.null(synergy_thr)) synergy_thr = min(stat_1D_rel2D)
+  if(is.null(synergy_thr)) synergy_thr = min(MDFS_1D_result$statistic[MDFS_1D_result$relevant.variables])
   partner_data_frame$IG_increase= partner_data_frame$rel_IG - stat_1D_rel2D
   partner_data_frame$synergy= (partner_data_frame$IG_increase > synergy_thr)
   partner_data_frame
@@ -191,6 +117,74 @@ find_true_partners<- function(partner_data_frame, MDFS_1D_result,
 
 only_partners<- function( annotated_partner_df, MDFS_1D_result  ) {
 	stopifnot(all(c("IG_increase", "synergy") %in% colnames(annotated_partner_df)))
-	annotated_partner_df$partner_name [ 
+	true_synergistic<-annotated_partner_df$partner_name [ 
 		annotated_partner_df$synergy ] |> unlist() |> as.character()
+	setdiff(true_synergistic, annotated_partner_df$rel_name)
+}
+
+
+#function performs 1D & 2D analysis jointly
+#for 2 discretization methods for features in X (based on 0, or median of each col in X)
+#
+# mc - minimum binarized feature class size, if it is less than mc, feature is filtered out.
+#
+#output of each of the 4 analysis variants
+#is stored in a corresponding sublist:
+#res_1Dm, res_2Dm, res_1D0, res_2D0 (two for median based and two for zero based)
+#
+#each sublist contains the fields mentioned at the beginning of the file.
+#
+#
+######moreover, each of the 2D sublists contains additional components:
+######	- partner_df: data.frame with one row per each feature in rel_set
+######		      where information on the best fitting, synergistic partner is given
+######	- partner_set:names of features that were found as synergistic partners,
+######		      but not found relevant by themselves
+
+MDFS_FS<- function(X, y, lvl=0.05,
+		   p.adjust.method="holm",
+		   seed=NULL,
+		   mc=30){
+   attributes(y)<-NULL
+   binarize_taxa(X,binarize_on_median = TRUE)|> filter_sparse_cols(thr=mc) -> Xm
+   
+   binarize_taxa(X,binarize_on_median = FALSE)|> filter_sparse_cols(thr=mc) -> X0
+   to_keep<- c("statistic","p.value","adjusted.p.value","relevant.variables")
+   MDFS.discrete(data=Xm,decision = y,dimensions = 1,
+                      p.adjust.method = p.adjust.method,
+		      level=0.05,seed=seed)[to_keep]-> res_1Dm
+   MDFS.discrete(data=Xm,decision = y,dimensions = 2,
+                      p.adjust.method = p.adjust.method,
+		      level=0.05,seed=seed)[to_keep]-> res_2Dm
+   MDFS.discrete(data=X0,decision = y,dimensions = 1,
+                      p.adjust.method = p.adjust.method,
+		      level=0.05,seed=seed)[to_keep]-> res_1D0
+   MDFS.discrete(data=X0,decision = y,dimensions = 2,
+                      p.adjust.method =p.adjust.method, 
+		      level=0.05,seed=seed)[to_keep]-> res_2D0
+  
+   res_1Dm$rel_set= colnames(Xm)[res_1Dm$relevant.variables]
+   res_2Dm$rel_set= colnames(Xm)[res_2Dm$relevant.variables]
+   res_1D0$rel_set= colnames(X0)[res_1D0$relevant.variables]
+   res_2D0$rel_set= colnames(X0)[res_2D0$relevant.variables]
+
+   res_2Dm$partner_df= get_interaction_partner(dataset=Xm, decision=y,
+					       	interesting.vars= res_2Dm$relevant.variables)
+   res_2Dm$partner_df= find_true_partners( partner_data_frame= res_2Dm$partner_df, 
+					   MDFS_1D_result= res_1Dm)
+   res_2Dm$partner_set=only_partners(annotated_partner_df= res_2Dm$partner_df, res_1Dm)
+
+   res_2D0$partner_df= get_interaction_partner(dataset=X0, decision=y,
+					       	interesting.vars= res_2D0$relevant.variables)
+   res_2D0$partner_df= find_true_partners( partner_data_frame= res_2D0$partner_df, 
+					   MDFS_1D_result= res_1D0)
+   res_2D0$partner_set=only_partners(annotated_partner_df= res_2D0$partner_df, res_1D0)
+
+   RESULT<-list(
+     res_1Dm=res_1Dm,
+     res_2Dm=res_2Dm,
+     res_1D0=res_1D0,
+     res_2D0=res_2D0
+   )
+   
 }
